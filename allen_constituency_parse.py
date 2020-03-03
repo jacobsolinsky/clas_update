5a93adc8c22598f6f5d6b514e48fc2c5846f8c3a
jacobsolinsky <jacobsolinsky@gmail.com>
2020-03-03 16:12:54 -0600

    First complete version.

--- allen_constituency_parse.py
+++ allen_constituency_parse.py
@@ -12,6 +12,7 @@ from allennlp.service.predictors import Predictor
 archive = load_archive(
             "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo-constituency-parser-2018.03.14.tar.gz"
         )
+
 def constituency_parse(sentence):
     predictor = Predictor.from_archive(archive, 'constituency-parser')
     return predictor.predict(sentence)
--- depth.py
+++ depth.py
@@ -9,45 +9,46 @@ import allen_constituency_parse
 import allen_dependency_parse
 from functools import lru_cache
 from graphviz import Digraph
+import spacy
 
+sentence_splitter = spacy.load('en_core_web_sm')
 
 class ConstituencyTree:
     ID_NO = 0
     def __init__(self, node_type, children=None, word=None):
         self.id = ConstituencyTree.ID_NO
         ConstituencyTree.ID_NO += 1
+        #Node type is whether a node is an S, NP, JJ, etc.
         self.node_type = node_type
         self.children = children
         self.parent = None
-        self.leaves = []
         self.is_leaf = False
-        if self.children:
+        if not self.children:
+            self.is_leaf = True
+        else:
+            #Linking child nodes to their parents is essential
+            #for the Frazier and Yngve algorithms
             for child in self.children:
                 child.parent = self
-                self.leaves += child.leaves
-        else:
-            self.leaves = [self]
-            self.is_leaf = True
+        #This attaches leaf nodes to the word they correspond with, i.e.: JJ: 'red'
         self.word = word
-        self.nodal_yngve = 0
-        self.nodal_frazier = 0
     
     def __repr__(self):
+        #The repr displays as a nested list if pasted into a browser
         inner = self.word if self.word else ''.join(
                 [f'<li>{child}</li>' for child in self.children]
                 )
         return f'<ol>{self.node_type}: {inner}</ol>'
     
     def frazier_yngve_graph(self, dot = None, parent = None):
+        #This generates a graph of the constituency tree with frazier and yngve score details.
         final = True
         if not dot:
-            self.calculate_yngve_score()
-            self.calculate_frazier_score()
             dot = Digraph()
         else:
             final = False
         content = f'''{self.node_type}: {self.word}
-            {self.yngve_score}|{self.frazier_score}''' \
+            {self.leaf_yngve}|{self.leaf_frazier}''' \
             if self.is_leaf else f'''{self.node_type}
             {self.nodal_yngve}|{self.nodal_frazier}'''
         dot.node(str(self.id), content)
@@ -58,98 +59,196 @@ class ConstituencyTree:
                 child.frazier_yngve_graph(dot, self.id)
         if final:
             dot.render('frazier-yngve-plot.gv', view=True)
-        
-        
+
+    @property
+    def nodal_yngve(self):
+        #This is equivalent to how many places to the left of the rightmost node
+        #A given node is among its siblings, starting at 0
+        if not hasattr(self, '_nodal_yngve'):
+            if self.parent:
+                for i, child in enumerate(self.parent.children[::-1]):
+                    child._nodal_yngve = i
+            else:
+                self._nodal_yngve = 0
+        return self._nodal_yngve
     
-    def calculate_yngve_score(self):
-        self.calculate_nodal_yngve()
-        self.calculate_leaf_yngve()
-    
-    def calculate_nodal_yngve(self):
-        if not self.is_leaf:
-            for i, child in enumerate(self.children[::-1]):
-                print(i)
-                print(child)
-                child.nodal_yngve = i
-                child.calculate_nodal_yngve()
-    
-    def calculate_leaf_yngve(self):
-        for leaf in self.leaves:
-            leaf.yngve_score = leaf.nodal_yngve
-            handle = leaf
+    @property
+    def leaf_yngve(self):
+        #This is the sum of the nodal_yngve scores of a node and all of its ancestors
+        if not hasattr(self, '_leaf_yngve'):
+            self._leaf_yngve = self.nodal_yngve
+            handle = self
             while handle.parent:
-                leaf.yngve_score += handle.parent.nodal_yngve
+                self._leaf_yngve += handle.parent.nodal_yngve
                 handle = handle.parent
-                
-    def calculate_frazier_score(self):
-        self.calculate_nodal_frazier()
-        self.calculate_leaf_frazier()
+        return self._leaf_yngve
+    
+    @property
+    def leaves(self):
+        if not hasattr(self, '_leaves'):
+            if self.is_leaf:
+                self._leaves = [self]
+            else:
+                self._leaves = [leaf for child in self.children 
+                                for leaf in child.leaves]
+        return self._leaves
             
-    def calculate_nodal_frazier(self):
-        if not self.is_leaf:
-            self.children[0].nodal_frazier = 1.5 if self.node_type == "S" else 1
-            for child in self.children:
-                child.calculate_nodal_frazier()
-            
-    def calculate_leaf_frazier(self):
-        for leaf in self.leaves:
-            leaf.frazier_score = leaf.nodal_frazier
-            handle = leaf
+    @property
+    def nodal_frazier(self):
+        #This is 0 if a node is not the leftmost of its siblings, 1.5 if it is
+        #and is the child of an S node, and 1 otherwise
+        if not hasattr(self, '_nodal_frazier'):
+            if self.parent:
+                if self.parent.children.index(self) == 0:
+                    if self.parent.node_type == "S":
+                        self._nodal_frazier = 1.5
+                    else:
+                        self._nodal_frazier = 1
+                else:
+                    self._nodal_frazier = 0
+            else:
+                self._nodal_frazier = 0
+        return self._nodal_frazier
+    
+    @property
+    def leaf_frazier(self):
+        #This is the sum of the node and all of its ancestors' nodal frazier scores
+        #Stopping when an ancester with nodal score of 0 is reached
+        if not hasattr(self, '_leaf_frazier'):
+            self._leaf_frazier = self.nodal_frazier
+            handle = self
             while handle.parent:
                 if handle.parent.nodal_frazier == 0: break
-                leaf.frazier_score += handle.parent.nodal_frazier
+                self._leaf_frazier += handle.parent.nodal_frazier
                 handle = handle.parent
+        return self._leaf_frazier
+    
+    @property
+    def num_clauses(self):
+        if not hasattr(self, '_num_clauses'):
+            is_clause = 1 if self.node_type == 'S' else 0
+            if self.children:
+                self._num_clauses = sum([child.num_clauses for child
+                                      in self.children]) + is_clause
+            else:
+                self._num_clauses = is_clause
+        return self._num_clauses
+    
+    @property
+    def total_words(self):
+        return len(self.leaves)
+
+    @property
+    def verb_count(self):
+        if not hasattr(self, '_verb_count'):
+            self.tally_parts_of_speech()
+        return self._verb_count
+    
+    @property
+    def adj_count(self):
+        if not hasattr(self, '_adj_count'):
+            self.tally_parts_of_speech()
+        return self._adj_count
+
+    @property
+    def noun_count(self):
+        if not hasattr(self, '_noun_count'):
+            self.tally_parts_of_speech()
+        return self._noun_count
+
+    @property
+    def adverb_count(self):
+        if not hasattr(self, '_adverb_count'):
+            self.tally_parts_of_speech()
+        return self._adverb_count
+
+    @property
+    def det_count(self):
+        if not hasattr(self, '_det_count'):
+            self.tally_parts_of_speech()
+        return self._det_count
+
+    @property
+    def personal_pronoun_count(self):
+        if not hasattr(self, '_personal_pronoun_count'):
+            self.tally_parts_of_speech()
+        return self._personal_pronoun_count
+
+    @property
+    def conj_count(self):
+        if not hasattr(self, '_conj_count'):
+            self.tally_parts_of_speech()
+        return self._conj_count
+
+    @property
+    def prep_count(self):
+        if not hasattr(self, '_prep_count'):
+            self.tally_parts_of_speech()
+        return self._prep_count
+
+    @property
+    def properN_count(self):
+        if not hasattr(self, '_properN_count'):
+            self.tally_parts_of_speech()
+        return self._properN_count
+                    
     
     def tally_parts_of_speech(self):
-        self.total_words = len(self.leaves)
-        self.noun_count, self.adj_count, self.adverb_count, \
-            self.verb_count, self.det_count, self.conj_count, \
-            self.prep_count, self.properN_count \
-            = 0, 0, 0, 0, 0, 0, 0, 0
-        type_dict = {'V': 'verb_count',
-                     'JJ': 'adj_count',
-                     'N': 'noun_count',
-                     'RB': 'adverb_count',
-                     'DT': 'det_count',
-                     'PRP': 'personal_pronoun_count',
-                     'CC': 'conj_count',
-                     'IN': 'prep_count',
-                     'NNP': 'properN_count',
+        self._noun_count, self._adj_count, self._adverb_count, \
+            self._verb_count, self._det_count, self._personal_pronoun_count,\
+            self._conj_count, self._prep_count, self._properN_count \
+            = 0, 0, 0, 0, 0, 0, 0, 0, 0
+        type_dict = {'V': '_verb_count',
+                     'JJ': '_adj_count',
+                     'N': '_noun_count',
+                     'RB': '_adverb_count',
+                     'DT': '_det_count',
+                     'PRP': '_personal_pronoun_count',
+                     'CC': '_conj_count',
+                     'IN': '_prep_count',
+                     'NNP': '_properN_count',
         }
         for leaf in self.leaves:
             for k, v in type_dict.items():
                 if leaf.node_type.startswith(k):
                     setattr(self, v, getattr(self, v) + 1)
     
-    def calculate_clause_count(self, init=True, count=0):
-        if self.node_type == 'S':
-            count += 1
-        if not self.is_leaf:
-            for child in self.children:
-                child.calculate_clause_count(False, count)
-        if init:
-            self.clause_count = count
-    
-    def calculate_mean_and_total(self):
-        self.total_Ydepth = 0
-        self.total_Fdepth = 0
-        for leaf in self.leaves:
-            yngve_total += leaf.yngve_score
-            frazier_total +=leaf.frazier_score
-        self.mean_Ydepth = self.total_Ydepth / len(self.leaves)
-        self.mean_Fdepth = self.total_Fdepth / len(self.leaves)
+    @property
+    def total_Ydepth(self):
+        #Sum of the leaf yngve scores of all of the leaves in this tree
+        if not hasattr(self, '_total_Ydepth'):
+            self._total_Ydepth = sum([leaf.leaf_yngve for leaf in self.leaves])
+        return self._total_Ydepth
 
-        
+    @property
+    def total_Fdepth(self):
+        #Sum of the leaf frazier scores of all of the leaves in this tree
+        if not hasattr(self, '_total_Fdepth'):
+            self._total_Fdepth =  sum([leaf.leaf_frazier for leaf in self.leaves])
+        return self._total_Fdepth
     
-    
+    @property
+    def mean_Ydepth(self):
+        #Mean yngve score of the leaves
+        return self.total_Ydepth / len(self.leaves)
+
+    @property
+    def mean_Fdepth(self):
+        #mean frazier score of the leaves
+        return self.total_Fdepth / len(self.leaves)
+
     @staticmethod
     def constituency_parse(sentence, engine='allennlp'):
+        #Generates the constituency parse tree, delegating to an engine which actually
+        #does the parsing
         return {'allennlp': ConstituencyTree.allen_constituency_parse}[
                 engine
                 ](sentence)
     
     @classmethod
     def allen_constituency_parse(cls, sentence):
+        #Generates the allennlp parse tree. This parse tree requires further
+        #translation to create an instance of the ConstituencyTree class
         return ConstituencyTree.allen_constituency_process(
                     allen_constituency_parse.constituency_parse(sentence)[
                     'hierplane_tree'
@@ -158,6 +257,8 @@ class ConstituencyTree:
     
     @classmethod
     def allen_constituency_process(cls, parse_dict):
+        #Translates the allennlp constituency parsetree into a Constituency
+        #Tree instance
         node_type = parse_dict['nodeType']
         children = None
         if 'children' in parse_dict.keys():
@@ -169,20 +270,8 @@ class ConstituencyTree:
         return ConstituencyTree(node_type, children)
 
 
-    
-    def calculate_yngve_depth(self, passdown = 0):
-        passdown = passdown + self.yngve_depth
-        if not self.children:
-            self.total_yngve_depth = passdown + self.yngve_depth
-        for i, child in enumerate(self.children[::-1]):
-            child.yngve_depth = i
-            child.calculate_yngve_depth(passdown)
-    
-    def calculate_frazier_depth(self):
-        pass
-
-
 class DependencyTree:
+    #this class represents itself as a list of relations with no further structure
     def __init__(self):
         self.relations = []
         self.start_positions = []
@@ -205,21 +294,30 @@ class DependencyTree:
                 })
     
     @property
-    @lru_cache(maxsize=1)
-    def average_distance(self):
-        i = 0
-        for r in self.relations:
-            i += abs(r['head_pos'] - r['tail_pos'])
-        return i / len(self.relations)
+    def total_SynDepLen(self):
+        #sum of the length of the relations in this dependency tree
+        if not hasattr(self, '_total_SynDepLen'):
+            self._total_SynDepLen = sum([
+                   abs(r['head_pos'] - r['tail_pos']) for r in self.relations 
+                    ])
+        return self._total_SynDepLen
+    
+    @property
+    def mean_SynDepLen(self):
+        #mean length of the relations in this dependency tree
+        return self.total_SynDepLen / len(self.relations)
     
     @staticmethod
     def dependency_parse(sentence, engine='allennlp'):
+        #generates the dependency tree with a given engine
         return {'allennlp': DependencyTree.allen_dependency_parse}[
                 engine
                 ](sentence)
     
     @classmethod
     def allen_dependency_parse(cls, sentence):
+        #generates an allennlp parsed dependency tree
+        #further translation is required to create a DependencyTree instance
         return DependencyTree.allen_dependency_process(
                     allen_dependency_parse.dependency_parse(sentence)[
                     'hierplane_tree'
@@ -227,6 +325,8 @@ class DependencyTree:
                 )
         
     def preprocess(self, parse_dict):
+        #Translates character positions of leaf nodes into word positions.
+        #Allennlp does not generate word positions when parsing.
         self.start_positions.append(parse_dict['spans'][0]['start'])
         if 'children' in parse_dict.keys():
             for child in parse_dict['children']:
@@ -234,6 +334,7 @@ class DependencyTree:
     
     @classmethod
     def allen_dependency_process(cls, parse_dict, dependency_tree = None):
+        #translates allennlp dependency tree into an instance of DependencyTree
         if 'children' not in parse_dict.keys(): return
         if not dependency_tree:
             dependency_tree = DependencyTree()
@@ -251,6 +352,38 @@ class DependencyTree:
             cls.allen_dependency_process(child, dependency_tree)
         return dependency_tree
 
-def get_statistics(sentences):
-    for i, sentence in sentences:
+def get_statistics(sentence, engine='allennlp'):
+    #Gets all of the statistics printed out in the example files for a given sentence
+    ct = ConstituencyTree.constituency_parse(sentence, engine)
+    dt = DependencyTree.dependency_parse(sentence, engine)
+    return {
+            'sentence_text': sentence,
+            'num_clauses': ct.num_clauses,
+            'mean_Fdepth': ct.mean_Fdepth,
+            'total_Fdepth': ct.total_Fdepth,
+            'mean_Ydepth': ct.mean_Ydepth,
+            'total_Ydepth': ct.total_Ydepth,
+            'mean_SynDepLen': dt.mean_SynDepLen,
+            'total_SynDepLen': dt.total_SynDepLen,
+            'noun_count': ct.noun_count,
+            'adj_count': ct.adj_count,
+            'adverb_count': ct.adverb_count,
+            'verb_count': ct.verb_count,
+            'det_count': ct.det_count,
+            'conj_count': ct.conj_count,
+            'prep_count': ct.prep_count,
+            'properN_count': ct.properN_count,
+    }
+
+def process_sentences(text):
+    #Breaks up a text into sentences and gets statistics and returns a printable representation
+    #of the collected statistics.
+    sentences = sentence_splitter(text).sents
+    dicts = [{**get_statistics(str(sentence)), **{'sentenceID': i}}
+        for i, sentence in enumerate(sentences)
+    ]
+    return f"""sentenceID|sentence_text|num_clauses|mean_Fdepth|total_Fdepth|mean_Ydepth|total_Ydepth|mean_SynDepLen|total_SynDepLen|noun_count|adj_count|adverb_count|verb_count|det_count|conj_count|prep_count|properN_count
+    {"".join([f'''{s["sentenceID"]}|{s["sentence_text"]}|{s["num_clauses"]}|{s["mean_Fdepth"]}|{s["total_Fdepth"]}|{s["mean_Ydepth"]}|{s["total_Ydepth"]}|{s["mean_SynDepLen"]}|{s["total_SynDepLen"]}|{s["noun_count"]}|{s["adj_count"]}|{s["adverb_count"]}|{s["verb_count"]}|{s["det_count"]}|{s["conj_count"]}|{s["prep_count"]}|{s["properN_count"]}
+    '''
+    for s in dicts])}"""
         
